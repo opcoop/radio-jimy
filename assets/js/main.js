82695f989c2b0c3b421be090857ef827ecd614be
Niv Sardi <xaiki@evilgiggle.com>
2014-06-19 00:59:29 -0300

    add basic radio jimy functionalities
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,18 +4,82 @@ require(['preloadjs', 'jquery', 'd3', 'topojson', 'underscore', 'utils'], functi
 	loadQueue.on('fileprogress', loadProgressUpdate);
 	loadQueue.on('complete', loadComplete);
 	loadQueue.loadFile({name: 'Topology', id: 'topo', src: 'assets/data/topo.json'});
+        loadQueue.loadFile({name: 'Countries (lowres)', id: 'countrieslowres', src: 'assets/data/world-countries.json'});
+        loadQueue.loadFile({name: 'Provincias)', id: 'provincias', src: 'assets/data/argentina-provincias.json'});
+
 	loadQueue.loadFile({name: 'ISO3166 Codes', id: 'iso3166', src: 'assets/data/iso3166.csv'});
 	loadQueue.loadFile({name: 'Continents', id: 'continents', src: 'assets/data/country_continent.csv'});
 	loadQueue.loadFile({name: 'Countries', id: 'countries', src: 'assets/data/country_latlon.csv'});
+
 	loadQueue.loadFile({name: 'Regions', id: 'regions', src: 'assets/data/region_codes.csv'});
 	loadQueue.loadFile({name: 'States', id: 'states', src: 'assets/data/state_latlon.csv'});
 	loadQueue.loadFile({name: 'Servers', id: 'servers', src: 'assets/data/servers.json'});
 	//loadQueue.loadFile({name: 'Tiles', id: 'tiles', src: 'tiles/geoserver.mbtiles'});
 
 	// D3 Elems
+
+        var config = {
+                scale: 1200,
+                rot: [55, 30]
+        };
+
 	var dMap = d3.select('#map'),
-		dProjection = d3.geo.mercator().scale(300).rotate([-10, 0]),
-		dPath = d3.geo.path().projection(dProjection);
+	    dProjection = d3.geo.orthographic()
+                    .scale(config.scale)
+                    .rotate(config.rot)
+                    .clipAngle(90),
+	    dPath = d3.geo.path().projection(dProjection),
+            dDrag = d3.behavior.drag()
+                    .on("drag", drag),
+            dZoom = d3.behavior.zoom()
+                    .translate([0, 0])
+                    .scale(1)
+                    .scaleExtent([1, 8])
+                    .on("zoom", zoomed);
+
+        dDrag.on('dragstart', dragstart);
+        dDrag.on('dragend', dragend);
+
+        function dragstart() {
+                drawMapLowRes();
+        }
+
+        function dragend() {
+                drawMap();
+        }
+
+        function drag() {
+                var s = config.scale/dProjection.scale();
+                var oO = dProjection.rotate();
+                dProjection.rotate([oO[0] + d3.event.dx*s / 6,
+                                    oO[1] - d3.event.dy*s / 6]);
+                refresh();
+        }
+
+        function zoomed() {
+                dProjection.scale(config.scale * d3.event.scale);
+                refresh();
+        }
+
+        dMap
+                .call(dDrag)
+                .call(dZoom) // delete this line to disable free zooming
+                .call(dZoom.event);
+
+        function hPath(d) {
+                return dPath(d) || "M0,0";
+        }
+
+        function refresh(duration) {
+                var feature = dMap.selectAll("path");
+                var markers = dMap.selectAll("circle");
+                (duration ? feature.transition().duration(duration) : feature).attr("d", hPath);
+                (duration ? markers.transition().duration(duration) : markers)
+                	.attr('cx', function(d) {
+                                return dProjection(d.geometry.coordinates)[0]; })
+			.attr('cy', function(d) {
+                                return dProjection(d.geometry.coordinates)[1]; });
+        }
 
 	// Local Vars
 	var mapLayers = [];
@@ -33,9 +97,11 @@ require(['preloadjs', 'jquery', 'd3', 'topojson', 'underscore', 'utils'], functi
 	}
 
 	function loadComplete(e) {
+                addMapLayer();
 		drawMap();
+                drawProv();
 		drawServers();
-		drawDistances();
+//		drawDistances();
 		$loading.fadeOut();
 		$cover.fadeOut();
 
@@ -57,45 +123,98 @@ require(['preloadjs', 'jquery', 'd3', 'topojson', 'underscore', 'utils'], functi
 		});*/
 	}
 
+        function addMapLayer() {
+                var baseMapLayer = dMap.append('g')
+			    .attr('id', 'layer1');
+
+        }
+
 	function drawMap() {
 		var topo = loadQueue.getResult('topo'),
-			countries = topojson.feature(topo, topo.objects.ne_50m_admin_0_countries),
-			states = topojson.feature(topo, topo.objects.ne_50m_admin_1_states_provinces_lakes);
+		    countries = topojson.feature(topo, topo.objects.ne_50m_admin_0_countries);
 
-		var baseMapLayer = dMap.append('g')
-								.attr('id', 'layer1');
+                var baseMapLayer = dMap.select('#layer1');
 
-		baseMapLayer.selectAll('.country')
-			.data(countries.features)
-		  .enter().append('path')
-		  	.attr('class', function(d) { return 'country ' + d.properties.iso_a2; })
+                var join = baseMapLayer.selectAll('path')
+			    .data(countries.features, function(d) { return d.properties.iso_a3; });
+
+                console.log ('map', countries.features[0]);
+		join.enter().append('path')
+		  	.attr('class', function(d) { return 'country ' + d.properties.iso_a3; })
 			.attr('d', dPath)
+                        .append('svg:title')
+                        .text(function(d) {return d.properties.name});
 
-		baseMapLayer.selectAll('.state')
-			.data(_.filter(states.features, function(d) { return d.properties.iso_a2 == 'US' }))
-		  .enter().append('path')
-		  	.attr('class', function(d) { return 'state ' + d.properties.code_hasc.replace('.', '_'); })
+                join.exit().remove();
+	}
+
+	function drawMapLowRes() {
+                var countries = loadQueue.getResult('countrieslowres');
+
+                var baseMapLayer = dMap.select('#layer1');
+		var join = baseMapLayer.selectAll('path')
+			    .data(countries.features, function(d) { return d.id; });
+
+                console.log ('mapLR', countries.features[0]);
+		join.enter().append('path')
+		  	.attr('class', function(d) { return 'country ' + d.id; })
 			.attr('d', dPath);
 
-		mapLayers.push(baseMapLayer);
+                join.exit().remove();
 	}
+
+        function drawProv() {
+                var provs = loadQueue.getResult('provincias');
+                var provLayer = dMap.append('g')
+			    .attr('id', 'layer2');
+
+		var join = provLayer
+                            .append('svg:path')
+                            .datum(topojson.mesh(provs, provs.objects.provincias, function(a, b) { return a !== b; }))
+                            .attr("class", "mesh")
+                            .attr("d", dPath);
+
+        }
 
 	function drawServers() {
                 var data = loadQueue.getResult('servers');
 		var servers = topojson.feature(data, data.objects.places).features;
 
 		var serversLayer = dMap.append('g')
-							.attr('id', 'layer2');
-
-		serversLayer.selectAll('.marker')
+			    .attr('id', 'layer3');
+		var circles = serversLayer.selectAll('.marker')
 			.data(servers)
-		  .enter().append('circle')
-		  	.attr('class', 'marker')
+		            .enter().append('circle');
+
+		circles.attr('class', 'marker')
 		  	.attr('data-dccode', function(d) { return d.properties.description; })
-			.attr('r', 4.5)
+			.attr('r', 0)
 			.attr('cx', function(d) { return dProjection(d.geometry.coordinates)[0]; })
 			.attr('cy', function(d) { return dProjection(d.geometry.coordinates)[1]; })
+                        .transition().duration(2000)
+                        .attr('r', 4.5);
+
+                circles.append('svg:title')
+                        .text(function(d) {return d.properties.name});
+
 			//.on('click', updateServers);
+
+                circles.on('mouseover', function () {
+                        d3.select(this).transition().duration(400)
+                                .attr('r', 10);
+                });
+
+                circles.on('mouseout', function () {
+                        d3.select(this).transition().duration(200)
+                                .attr('r', 4.5);
+                });
+
+
+                circles.on('clicked', function () {
+                        d3.select(this).transition().duration(4000)
+                                .attr('r', 20);
+                });
+
 
 		mapLayers.push(serversLayer);
 	}
@@ -118,47 +237,7 @@ require(['preloadjs', 'jquery', 'd3', 'topojson', 'underscore', 'utils'], functi
 							.attr('id', 'layer3');
 
 		_.each(countries, function(country) {
-
-			if(country['iso 3166 country'] == 'US')
-			{
-				var regions = _.filter(d3.csv.parse(loadQueue.getResult('regions')), function(reg) { return reg.country == 'US' }),
-					states = _.filter(d3.csv.parse(loadQueue.getResult('states')), function(state) { return _.any(regions, function(reg) { return reg.region == state.state })});
-
-				_.each(states, function(state) {
-					var dState = dMap.select('.US_'+state.state).data()[0];
-
-					if(!dState)
-						return;
-
-					var dc = _.sortBy(servers, function(server) {
-						return Util.Geo.getDistance(server.geometry.coordinates, [state.longitude, state.latitude]);
-					})[0];
-
-					var source = dProjection(dc.geometry.coordinates),
-						target = dPath.centroid(dState);
-
-					if(Util.Geo.inBounds(dPath.bounds(dState), source))
-					{
-						/*distanceLayer.append('circle')
-						  	.attr('class', 'arc')
-							.attr('r', 10)
-							.attr('cx', function(d) { return source[0]; })
-							.attr('cy', function(d) { return source[1]; });*/
-					} else {
-						distanceLayer.append('path')
-							.attr('class', 'arc')
-							.attr('d', function() {
-								var dx = target[0] - source[0],
-						        	dy = target[1] - source[1],
-						        	dr = Math.sqrt(dx * dx + dy * dy);
-							    return "M" + source[0] + "," + source[1] + "A" + dr + "," + dr + " 0 0,1 " + target[0] + "," + target[1];	
-							});
-					}
-				});
-
-			} else {
-
-				var dCountry = dMap.select('.'+country['iso 3166 country']).data()[0];
+			var dCountry = dMap.select('.'+country['iso 3166 country']).data()[0];
 
 				if(!dCountry)
 					return;
@@ -190,7 +269,6 @@ require(['preloadjs', 'jquery', 'd3', 'topojson', 'underscore', 'utils'], functi
 						    return "M" + source[0] + "," + source[1] + "A" + dr + "," + dr + " 0 0,1 " + target[0] + "," + target[1];	
 						});
 				}
-			}
 		});
 
 		mapLayers.push(distanceLayer);
