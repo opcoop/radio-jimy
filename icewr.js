893985ea8beba3e296504fd16156376fdd1f828b
Vladimir di Fiore Prieto <vsdifiore@anses.gov.ar>
2014-08-05 20:22:13 -0300

    cambios menores

--- icewr.js
+++ icewr.js
@@ -3,8 +3,9 @@ var Icecast = require('icecast-helper')
 ,   url     = require('url')
 ,   _	    = require('underscore')
 ;
+var text ="";
 
-var opt = require('node-getopt').create([
+var opt  = require('node-getopt').create([
         ['k' , 'key'		, 'API key'],
         ['h' , 'help'		, 'display this help'],
         ['v' , 'version'	, 'show version']
@@ -19,23 +20,28 @@ request(URL, function (error, response, body) {
         if (error || response.statusCode != 200) {
                 return error;
         }
-        var collection = JSON.parse (body);
+	        
+	var collection = JSON.parse (body);
         _.each(collection, function (item) { /* populate the memorystore */
                 if (! item.stream)
                         return;
 
-                var model = {
-                        name  : item.name,
-                        _id   : item._id,
-                        password: key,
-                };
-
-                console.log("<mount><mount-name>",item.name,"</mount-name>", "<username>source</username><password>", key, "</password>");
-
-             
-
-                return;
+              		
+		text += "<mount>\n"
+		text += "<mount-name>" + item._id + "</mount-name>\n"
+		text += "<username>source</username>\n"
+		text += "<password>" + item.password + "<password>\n" 
+		text += "<stream-name>" + item.name + "</stream-name>\n"
+		text += "<stream-description>FM" + item.freq + ", " + item.city + ", " + item.prov + "</stream-description>\n"
+    		text += "<stream-url>http://some.place.com</stream-url>\n"
+		text += "</mount>\n"
+		          
+		
+		return;
         });
+var fs   = require('fs');		
+fs.writeFileSync ('icecast.txt',text);		
+
 });
 
-console.log ('ran out of radios to monitor');
+
