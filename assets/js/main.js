73199489331710f8651c5b1230521e68696ad511
Niv Sardi <xaiki@evilgiggle.com>
2014-09-22 14:28:16 -0300

    hack arround race in load
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,30 @@ require(['loading', 'app', 'vent',
          'jquery', 'underscore', 'backbone', 'utils',
          'models/radio',
          'views/map', 'views/radiolist', 'views/player'],
-        function(loadQueue, App, Vent,
-                 $, _, Backbone, Util,
-                 Radio,
-                 MapView, ListView, PlayerView) {
+function(loadQueue, App, Vent,
+         $, _, Backbone, Util,
+         Radio,
+         MapView, ListView, PlayerView) {
 
-            loadQueue.on('complete', function () {
-                    var $loading = $('.loading'),
-	                $cover = $('.coverall');
-                    $loading.fadeOut();
-		    $cover.fadeOut();
-                    App.start();
-            });
+        var hack = false;
+        function start() {
+                hack = true;
+                var $loading = $('.loading'),
+	            $cover = $('.coverall');
+                $loading.fadeOut();
+		$cover.fadeOut();
+                App.start();
+        }
+        if (loadQueue.loaded && !hack) //XXX: hack
+                start();
 
-            Vent.on('radio:selected', function (model) {
+        loadQueue.on('complete', start);
 
+        Vent.on('radio:selected', function (model) {
                 return true;
-            });
+        });
 
-            App.vent.on('radio:selected', function (args) {
+        App.vent.on('radio:selected', function (args) {
                 console.log('hello poto', args);
-            });
+        });
 })
